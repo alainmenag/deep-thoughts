cb42701580b108779de3c41f44a982a4941f4034
Sebastian Alain Mena Galindo <alainmenag@gmail.com>
2023-06-20 18:35:15 -0400

    [1] Client-side login/sign up

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,8 @@ import React from 'react';
 import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
+import { setContext } from '@apollo/client/link/context';
+
 import Header from './components/Header';
 import Footer from './components/Footer';
 
@@ -16,9 +18,19 @@ import Signup from './pages/Signup';
 const httpLink = createHttpLink({
     uri: '/graphql',
   });
+
+  const authLink = setContext((_, { headers }) => {
+    const token = localStorage.getItem('id_token');
+    return {
+      headers: {
+        ...headers,
+        authorization: token ? `Bearer ${token}` : '',
+      },
+    };
+  });
   
   const client = new ApolloClient({
-    link: httpLink,
+    link: authLink.concat(httpLink),
     cache: new InMemoryCache(),
   });
 
