7374b7409a0196f8f7686019c79c408c9eb6e200
Sebastian Alain Mena Galindo <alainmenag@gmail.com>
2023-06-20 16:26:52 -0400

    [1] Set up mutations and JWT logic

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -5,8 +5,7 @@ const { gql } = require('apollo-server-express');
 //const typeDefs = gql``;
 
 const typeDefs = gql`
-
-type User {
+  type User {
     _id: ID
     username: String
     email: String
@@ -15,7 +14,7 @@ type User {
     friends: [User]
   }
 
-type Thought {
+  type Thought {
     _id: ID
     thoughtText: String
     createdAt: String
@@ -32,12 +31,26 @@ type Thought {
   }
 
   type Query {
+    me: User
     users: [User]
     user(username: String!): User
     thoughts(username: String): [Thought]
     thought(_id: ID!): Thought
   }
-  
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    addThought(thoughtText: String!): Thought
+    addReaction(thoughtId: ID!, reactionBody: String!): Thought
+    addFriend(friendId: ID!): User
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
 `;
 
 
